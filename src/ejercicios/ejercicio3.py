6ef7e43ab651066b4c4f318a0bfe97a0148c48eb
MARCOS PAULO PISSANI <pissanimarcos@gmail.com>
2026-04-21 16:33:24 -0300

    Update 2 files
    - /src/ejercicios/ejercicio3.py
    - /notebooks/ejercicio3.ipynb

--- src/ejercicios/ejercicio3.py
+++ src/ejercicios/ejercicio3.py
@@ -42,3 +42,65 @@ def coordenadas_invalidas (datasets_paths: Path):
         coordenadas_invalidas.extend(list(filter(revisar_coordenadas, archivo)))
 
     return len(coordenadas_invalidas), coordenadas_invalidas
+
+    # ---------------------Inciso D----------------------
+
+def registros_duplicados(datasets_paths: Path):
+    """
+    Args:
+        datasets_paths (Path): rutas de los archivos a buscar
+
+    Returns:
+        _type_: _description_
+    """
+
+    ids_vistos = set()
+    ids_duplicados = set()
+
+    for path in datasets_paths:
+        config, core = get_core_info(path)
+        path = path / core
+
+        archivo = manejo_archivos.get_archive(path, **config)
+
+        for dato in archivo:
+            id_dato = dato.get('ID')
+            if id_dato in ids_vistos:
+                ids_duplicados.add(id_dato)
+            else:
+                ids_vistos.add(id_dato)
+
+    return len(ids_duplicados), list(ids_duplicados)
+
+#---------------------Inciso E----------------------
+
+def country_codes_invalidos(datasets_paths: Path):
+    """
+    Args:
+        datasets_paths (Path): rutas de los archivos a buscar
+
+    Returns:
+        _type_: _description_
+    """
+
+    # caracteres alfabéticos en mayúscula, con una longitud de dos caracteres para identificar paises.
+    codigos_paises_validos = set(string.ascii_uppercase[i] 
+                                 + string.ascii_uppercase[j] 
+                                 for i in range(26) for j in range(26))
+
+    codigos_invalidos = set()
+
+    for path in datasets_paths:
+
+        config, core = get_core_info(path)
+        path = path / core
+
+        archivo = manejo_archivos.get_archive(path, **config)
+
+        for dato in archivo:
+            country_code = dato.get('countryCode')
+            # Verificar si el countryCode es válido y agregarlo a la lista de códigos inválidos si no lo es
+            if country_code and country_code not in codigos_paises_validos:
+                codigos_invalidos.add(country_code)
+
+    return list(codigos_invalidos)
