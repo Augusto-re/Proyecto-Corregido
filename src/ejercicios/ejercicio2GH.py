d1918679ee47f6da024a9452f1e8f8440873fc82
PeriGP <nico_guzzo_punta@hotmail.com>
2026-04-30 17:43:15 -0300

    ejercicios 2 G y H corregidos

--- src/ejercicios/ejercicio2GH.py
+++ src/ejercicios/ejercicio2GH.py
@@ -1,36 +1,58 @@
 from utils import manejo_archivos
 from utils.config_archivo import get_core_info
 from pathlib import Path
+
+
+
 #ejercicio 2G
 def valuesInColumn(dataset:Path, columns_name: str):
+    """retorna la cantidad de valores en la columna dada
 
+    Args:
+        dataset (Path): ruta al dataset
+        columns_name (str): nombre de la columna
+
+    Returns:
+        int: cantidad de valores únicos en la columna
+    """    
+    config, core = get_core_info(dataset)
+    archivo_path = dataset / core
     values = set()
-    headers = manejo_archivos.get_header(dataset)
-    if columns_name not in headers:
-        return f"la columna '{columns_name}' no existe en el archivo {archivo_path}"
-    for path in dataset:
-        config, core = get_core_info(path)
-        archivo_path = path / core
-        archivo = manejo_archivos.get_archive(archivo_path, **config)
-        values.update(archivo[columns_name])
+    archivo = manejo_archivos.get_archive(archivo_path, **config)
+
+    if columns_name not in archivo[0].keys():
+        return 0
+
+    for row in archivo:
+        values.update([row[columns_name]])
 
 
     return len(values)
 
-print(valuesInColumn("C:\Users\cacog\code\raw_datasets\Colección Ornitológica del IADIZA-CCT CONICET Mendoza IADIZA-COI\Colecci├│n Ornitol├│gica del IADIZA-CCT CONICET Mendoza IADIZA-COI\occurrence.txt","publisher"))
+
+
+
 
 #ejercicio 2H
-def valueFrecuenseInClolumn(dataset: Path, columns_name: str):
+def valueFrecuenseInColumn(dataset: Path, columns_name: str):
+    """retorna la frecuencia de cada valor en la columna dada
 
+    Args:
+        dataset (Path): ruta al dataset
+        columns_name (str): nombre de la columna
+
+    Returns:
+        dict: diccionario con la frecuencia de cada valor en la columna
+    """    
+    config, core = get_core_info(dataset)
+    archivo_path = dataset / core
+    values = set()
+    archivo = manejo_archivos.get_archive(archivo_path, **config)
     values_in_column = {}
 
-    for path in dataset:
-        config, core = get_core_info(path)
-        archivo_path = path / core
-        archivo = dict(manejo_archivos.get_archive(archivo_path, **config))
-        values_in_column = {}
-        for value in archivo[columns_name]:
-            values_in_column[value] = values_in_column.get(value, 0) + 1
+    for row in archivo:
+        value = row.get(columns_name)
+        values_in_column[value] = values_in_column.get(value, 0) + 1
 
 
     return values_in_column
