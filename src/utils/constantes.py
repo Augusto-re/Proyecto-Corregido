a8838da97a5b9c90d71f876600d84d0fd1e12570
MARCOS PAULO PISSANI <pissanimarcos@gmail.com>
2026-05-02 17:16:08 -0300

    Update 3 files
    - /notebooks/ejercicio4.ipynb
    - /src/utils/constantes.py
    - /src/ejercicios/ejercicios7.py

--- src/utils/constantes.py
+++ src/utils/constantes.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 PROJECT_PATH = Path(__file__).parent.parent.parent # Path a la raiz del archivo #
-DATASETS_PATH_RAW = PROJECT_PATH / "raw_datasets" # Path to the raw datasets folder
-DATASETS_PATH_PROCESSED = PROJECT_PATH / "processed_datasets" # Path to the processed datasets folder
+DATASETS_PATH_RAW = PROJECT_PATH / "raw_datasets" 
+DATASETS_PATH_PROCESSED = PROJECT_PATH / "processed_datasets" 
+LOGS_PATH = PROJECT_PATH / "logs" 
 
 # Fechas estandar en Darwin Core
 CAMPOS_FECHA_DWC = [
