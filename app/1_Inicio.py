4a74b66ba0795c467cedff1b17a43f574d492e0e
MARCOS PAULO PISSANI <pissanimarcos@gmail.com>
2026-05-02 15:42:09 -0300

    Update 4 files
    - /app/pages/2_Estado_del_sistema.py
    - /app/pages/3_Búsqueda.py
    - /app/pages/4_Visualización.py
    - /app/1_Inicio.py

---
+++ app/1_Inicio.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Bienvenido a la aplicación de análisis de datos")
+st.write("Esta aplicación te permite analizar y visualizar datos de biodiversidad utilizando la APP." \
+" Puedes ver y modificar los datasets de proporcionados por las instituciones Iadiza , Inaturalist y Xeno-Canto." \
+"Explora las diferentes secciones para descubrir datos interesantes sobre la biodiversidad .")
+st.write("")
+st.subheader("¿Que es el Estandar Darwin Core?")
+st.write("El Estandar Darwin Core es un estándar técnico mantenido por la organización que establece un " \
+"conjunto común de términos y definiciones para que científicos e instituciones de todo el mundo puedan " \
+"compartir información sin confusiones.")
+st.write("")
+st.subheader("Algunas Instrucciones :")
+st.write("- En el lado Izquiero tienes un menú de navegación para acceder a las diferentes secciones de la aplicación.")
+st.write("- En la sección 'Inicio' puedes encontrar información general sobre la aplicación y su propósito. [Aquí estás actualmente]")
+st.write("- En la sección 'Estado del sistema' puedes ver el estado actual de los datasets y realizar análisis de calidad de datos.")
+st.write("- En la sección 'Búsqueda de datos' puedes buscar registros específicos utilizando filtros y criterios de búsqueda.")
+st.write("- En la sección 'Visualización de datos' puedes explorar gráficos y visualizaciones interactivas para comprender mejor los datos.")
+st.write("")
